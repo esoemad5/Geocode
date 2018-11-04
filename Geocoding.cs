eaed12efe424dda11361f7f71e9df814cdf3238b
Elliott Soemadi <esoemad5@gmail.com>
2018-11-04 00:46:55 -0500

    Added Geocoding.cs
    Geocoding is a static class with one static method: GetLatitudeAndLongitude

---
+++ Geocoding.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api_Test
+{
+    public static class Geocode
+    {
+        public static float[] GetLatitudeAndLongitude(string address, string key)
+        {
+            using (var webClient = new WebClient())
+            {
+                string rawJson = webClient.DownloadString("https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=" + key);
+                GeocodePartialJson geocode = JsonConvert.DeserializeObject<GeocodePartialJson>(rawJson);
+                return new float[] { geocode.results[0].geometry.location.lat, geocode.results[0].geometry.location.lng };
+            }
+        }
+
+        private class GeocodePartialJson
+        {
+            public Results[] results;
+            public class Results
+            {
+                public Geometry geometry;
+                public class Geometry
+                {
+                    public Location location;
+                    public class Location
+                    {
+                        public float lat;
+                        public float lng;
+                    }
+                }
+            }
+        }
+    }
+}
